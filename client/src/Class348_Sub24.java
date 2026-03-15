19c7cf9bb27802d65e47a49e46e8ff7a03304dbb
GregHib <greghib@users.noreply.github.com>
2026-03-15 20:12:28 +0000

    Fix scroll zoom when ctrl shift teleporting

--- client/src/Class348_Sub24.java
+++ client/src/Class348_Sub24.java
@@ -326,7 +326,9 @@ final class Class348_Sub24 extends Class348 {
                         Class216.method1583((byte) -73);
                         Class311.anInt3918++;
                         if (Class320.overGameScreen && Class320.scrollWheelDiff != 0) {
-                            Class320.zoomStep += -Class320.scrollWheelDiff * Loader.ZOOM_OFFSET_STEP;
+                            if (Class192.anInt2581 <= 0 || !Class182.aClass346_2449.method2696(82, -125) || !Class182.aClass346_2449.method2696(81, -126)) {
+                                Class320.zoomStep += -Class320.scrollWheelDiff * Loader.ZOOM_OFFSET_STEP;
+                            }
                         }
                         if (Class237.aBoolean3103) {
                             Class98.anInt1568++;
