9c2827b26464b44223deef7beb43de795bd2025d
GregHib <greghib@users.noreply.github.com>
2026-06-11 17:33:08 +0100

    Remove unused SocketOpener.kt

--- src/commonMain/kotlin/SocketOpener.kt
+++
@@ -1,7 +0,0 @@
-interface SocketOpener {
-    @Throws(IOException::class)
-    fun openDirect(host: String, port: Int, key: Int): Class238
-
-    @Throws(IOException::class)
-    fun openViaProxy(host: String, port: Int, key: Int): Class238
-}
