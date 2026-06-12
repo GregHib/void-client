eca3b7d66d68fad16726c43816bcf8fc929c37db
GregHib <greghib@users.noreply.github.com>
2026-06-12 13:48:46 +0100

    Fix high detail particles setting introduced in 05b77c71

--- src/jvmMain/kotlin/Applet_Sub1.kt
+++ src/jvmMain/kotlin/Applet_Sub1.kt
@@ -325,6 +325,7 @@ abstract class Applet_Sub1 : Panel(), GameApplet, Runnable, FocusListener, Windo
                         /* empty */
                     }
                 }
+                RuntimeInfoProvider.instance = JvmRuntimeInfo()
                 aa_Sub3.method168(103.toByte())
                 Class127_Sub1.method1119(false)
                 method87((-97).toByte())
