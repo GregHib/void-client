efc65abd2bfbcba9e6c04d25fde91068e6f864ce
GregHib <greghib@users.noreply.github.com>
2026-08-07 20:07:51 +0100

    Move RegionTileNode

--- src/commonMain/kotlin/RegionTileNode.kt
+++ src/commonMain/kotlin/RegionTileNode.kt
@@ -1,5 +1,5 @@
 /* Class348_Sub7 - Decompiled by JODE
 * Visit http://jode.sourceforge.net/
 */
-class RegionTileNode internal constructor(@JvmField var anInt6649: Int, @JvmField var anInt6648: Int, @JvmField var anInt6645: Int, @JvmField var anInt6647: Int, @JvmField var anInt6642: Int, @JvmField var aBoolean6650: Boolean) : LinkedListNode() {
+class RegionTileNode(@JvmField var anInt6649: Int, @JvmField var anInt6648: Int, @JvmField var anInt6645: Int, @JvmField var anInt6647: Int, @JvmField var anInt6642: Int, @JvmField var aBoolean6650: Boolean) : LinkedListNode() {
 }
