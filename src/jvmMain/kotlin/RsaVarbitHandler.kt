9082fc2f467c064209518a35c1ae8cbb2422fbff
GregHib <greghib@users.noreply.github.com>
2026-08-18 21:59:37 +0100

    Fix ConnectionStateType

--- src/jvmMain/kotlin/RsaVarbitHandler.kt
+++ src/jvmMain/kotlin/RsaVarbitHandler.kt
@@ -90,7 +90,7 @@ class RsaVarbitHandler internal constructor(@JvmField var anInt4899: Int) : Conf
                 Direct3dModel.aString5420 = TimedTileQueueEntry.aConnectionStateType_9660!!.aLocalizedText_1012!!.method2063(ChatCommandProcessor.anInt6967, 544)
                 if (TimedTileQueueEntry.aConnectionStateType_9660!!.aBoolean1016) ConfigFlagUtilStatics.anInt3236 = (i * ((TimedTileQueueEntry.aConnectionStateType_9660!!.anInt1021) + -(TimedTileQueueEntry.aConnectionStateType_9660!!.anInt1025)) / 100 + (TimedTileQueueEntry.aConnectionStateType_9660!!.anInt1025))
                 if (TimedTileQueueEntry.aConnectionStateType_9660!!.aBoolean1013) Direct3dModel.aString5420 += ConfigFlagUtilStatics.anInt3236.toString() + "%"
-            } else if (TimedTileQueueEntry.aConnectionStateType_9660 == ConnectionStateTypeStatics.aConnectionStateType_1043) {
+            } else if (TimedTileQueueEntry.aConnectionStateType_9660 == ConnectionStateType.aConnectionStateType_1043) {
                 ActorEntity.aResourceLoaderThread_897 = null
                 ByteBufferStatics.method3379(2, 3)
             } else {
