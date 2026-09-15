25512e2243319a08ad3827a66e9257790de484b8
GregHib <greghib@users.noreply.github.com>
2026-09-15 23:06:46 +0100

    Fix bad cast causing crash on dungeoneering master cape emote

--- src/commonMain/kotlin/NpcSpawnDecoder.kt
+++ src/commonMain/kotlin/NpcSpawnDecoder.kt
@@ -15,7 +15,7 @@ object NpcSpawnDecoder {
 
     fun method858(bool: Boolean, player: Player) {
         anInt1526++
-        val class348_sub9 = ((CompletedResourceRequest.aHashtable_10465!!.method3480(player.anInt10290.toLong(), -6008)) as CharacterRenderState)
+        val class348_sub9 = ((CompletedResourceRequest.aHashtable_10465!!.method3480(player.anInt10290.toLong(), -6008)) as? CharacterRenderState)
         if (bool == true) {
             if (class348_sub9 == null) IdentKitRecolor.method1614(979190089, null, player.plane.toInt(), (player.anIntArray10317!![0]), (player.anIntArray10320!![0]), player, null, 0)
             else class348_sub9.method2781(21.toByte())
